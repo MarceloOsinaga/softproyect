c86dee7e8d0fd03e2ef4dda270b833ef6a8d8fcb
MarceloOsinaga <osinaga255@gmail.com>
2018-09-15 07:04:23 -0400

    Solucionado

--- resources/views/tipocontrato/partials/form.blade.php
+++ resources/views/tipocontrato/partials/form.blade.php
@@ -1,16 +1,16 @@
 
-<div class="row p-t-20">
-<div class="col-md-6">
+
 	<div class="form-group">
 	{!! Form::label('name', 'Nombre ', ['class' => 'control-label']) !!}
 	{!! Form::text('nombre', null, ['class' => 'form-control'],['placeholder' => 'Nombre'],['id' => 'Nombre']) !!}
 	</div>
-</div>
-<div class="col-md-6">
-	<div class="form-group">
-	{!! Form::label('descripcion', 'Descripción' , ['class' => 'control-label'],['placeholder' => 'Nombre'],['id' => 'Nombre']) !!}
 
-</div>
+
+	<div class="form-group">
+	{!! Form::label('descripcion', 'Descripción' , ['class' => 'control-label']) !!}
+	{!! Form::text('Descripcion', null, ['class' => 'form-control'],['placeholder' => 'Descripcion'],['id' => 'Descripcion']) !!}
+	</div>
+
 <hr>
 	<div class="form-actions">
 			{!!  Form::submit('Guardar', ['class' => 'btn  btn-outline-primary btn-rounded '] ) !!}
