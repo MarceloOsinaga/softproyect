4f5b1329587b0b5cf384f2282af2e964a2811fda
gmendozaribera <gmendozaribera@gmail.com>
2018-09-16 18:27:25 -0400

    causa

--- resources/views/causa/show.blade.php
+++ resources/views/causa/show.blade.php
@@ -1,29 +1,23 @@
-@extends('layouts.admin')
+@extends('layouts.formulario')
 
-@section('content')
- <div class="card-body">
-     <div class="row">
-   		<div class="col-lg-12">
-			<div class="card-header">
-				<h4 class="m-b-0 text-white">Edicion de Tipo causa</h4>
-			</div>
-				<div class="card-body">
-					<a href="{{ route('tipocausas.index') }}" class="btn btn-outline-primary btn-rounded pull-right">
-						Regresar</a>
-					<h3 class="card-title">Modifique los datos seleccionados</h3>
-						<hr>
+@section('titulo')
+ Perfil tipo de causa Actual
+@endsection
+
+@section('rutaRegresar')
+	href="{{ route('causas.index') }}"
+
+@endsection
+
+@section('formulario')
 						<strong>Id:</strong>
-						<p>{{ $tipocausa->id }}</p> 
+						<p>{{ $causa->id }}</p> 
 						<strong>Nombre:</strong>
-						<p>{{ $tipocausa->nombre }}</p>
+						<p>{{ $causa->nombre }}</p>
 						<strong>Descripcion:</strong>
-						<p>{{ $tipocausa->descripcion }}</p>
+						<p>{{ $causa->descripcion }}</p>
 						<hr>
-					<a href="{{ route('tipocausas.edit', $tipocausa->id) }}" class="btn  btn-outline-primary btn-rounded">
+					<a href="{{ route('causas.edit', $causa->id) }}" class="btn  btn-outline-primary btn-rounded">
 					Editar
 				</a>
-			</div>
-		</div>
-	</div>
-</div>
 @endsection
