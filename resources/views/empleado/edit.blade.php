5890bc05cb8d45debbff248a089d9e174c8e8e1d
MarceloOsinaga <osinaga255@gmail.com>
2018-09-16 21:31:45 -0400

    Crud Empleado

--- resources/views/empleado/edit.blade.php
+++ resources/views/empleado/edit.blade.php
@@ -8,15 +8,15 @@
 					<h4 class="m-b-0 text-white">Edicion de Tipo Contrato</h4>
 				</div>
 							<div class="card-body">
-									<a href="{{ route('tipocontratos.index') }}" class="btn btn-outline-primary btn-rounded pull-right">
+									<a href="{{ route('empleados.index') }}" class="btn btn-outline-primary btn-rounded pull-right">
 											Regresar
 										</a>
 									<h3 class="card-title">Datos</h3>
 									<hr>
-								@include('tipocontrato.partials.errors')
-								{!! Form::model($tipocontrato, ['route' => ['tipocontratos.update', $tipocontrato->id], 'method' => 'PUT']) !!}
+								@include('empleado.partials.errors')
+								{!! Form::model($empleado, ['route' => ['empleados.update', $empleado->id], 'method' => 'PUT']) !!}
 									
-									@include('tipocontrato.partials.form')
+									@include('empleado.partials.form')
 									
 								{!! Form::close() !!}
 				</div>
